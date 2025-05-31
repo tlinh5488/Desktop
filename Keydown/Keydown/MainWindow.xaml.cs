9d23ea7f90b153f242a8ef86e4b524f31c517b89
Tlinh_nbnp2305 <pttlinh020305@gmail.com>
2025-05-31 10:50:09 +0700

    wps

---
+++ Keydown/Keydown/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Keydown
+{
+        public partial class MainWindow : Window
+        {
+            public MainWindow()
+            {
+                InitializeComponent();
+            }
+
+            private void Grid_KeyDown(object sender, KeyEventArgs e)
+            {
+                MessageBox.Show("Grid handler, bubbling up");
+            }
+
+            private void Grid_PreviewKeyDown(object sender, KeyEventArgs e)
+            {
+                MessageBox.Show("Grid handler, tunneling down");
+                e.Handled = true; // CHẶN không cho sự kiện đi tiếp
+            }
+
+            private void rotatedButton_KeyDown(object sender, KeyEventArgs e)
+            {
+                MessageBox.Show("rotatedButton handler, bubbling up");
+            }
+
+            private void rotatedButton_PreviewKeyDown(object sender, KeyEventArgs e)
+            {
+                MessageBox.Show("rotatedButton handler, tunneling down");
+            }
+
+            private void Window_KeyDown(object sender, KeyEventArgs e)
+            {
+                MessageBox.Show("Window handler, bubbling up");
+            }
+
+            private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+            {
+                MessageBox.Show("Window handler, tunneling down");
+            }
+        }
+    }
+
---
+++ Twobuttons/Twobuttons/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media; 
+
+namespace TwoControls
+{
+    class TwoControls : Window
+    {
+        private TextBox txtName;
+        private const int MARGINSIZE = 10;
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application app = new Application();
+            app.Run(new TwoControls());
+        }
+
+        public TwoControls()
+        {
+            Title = "Demo Hai Điều Khiển";
+            Width = 288;
+
+            // Tạo StackPanel
+            StackPanel panel = new StackPanel();
+            Content = panel;
+            panel.Background = Brushes.Beige;
+            panel.Margin = new Thickness(MARGINSIZE);
+
+            // TextBox
+            txtName = new TextBox();
+            txtName.FontSize = 16;
+            txtName.HorizontalAlignment = HorizontalAlignment.Center;
+            txtName.Width = Width / 2;
+            txtName.Margin = new Thickness(MARGINSIZE);
+            panel.Children.Add(txtName);
+
+            // Button
+            Button btnGreet = new Button();
+            btnGreet.Content = "Chào";
+            btnGreet.FontSize = 16;
+            btnGreet.HorizontalAlignment = HorizontalAlignment.Center;
+            btnGreet.Margin = new Thickness(MARGINSIZE);
+            btnGreet.Click += ButtonOnClick;
+            panel.Children.Add(btnGreet);
+
+            // Tự động điều chỉnh chiều cao
+            SizeToContent = SizeToContent.Height;
+        }
+
+        void ButtonOnClick(object sender, RoutedEventArgs args)
+        {
+            MessageBox.Show("Hé Lô cục cưng, " + txtName.Text, "Lời chào");
+        }
+    }
+}
